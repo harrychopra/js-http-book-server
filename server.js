2548fe7191905499d83ee20a8a768a2cf0488ef1
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-01-31 03:00:00 +0000

    implement GET /api/books

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import http from 'http';
 import db from './db/db.js';
+import { string } from 'pg-format';
 
 const HOST = 'localhost';
 const PORT = 8080;
 
 const stringify = json => JSON.stringify(json);
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url: path } = req;
 
   // ***************************************************************************
@@ -19,6 +20,25 @@ const server = http.createServer((req, res) => {
     const data = { message: 'Hello' };
     try {
       res.write(stringify(data));
+    } catch (err) {
+      res.statusCode = 500;
+      const data = { error: err.message };
+      res.write(stringify(data));
+    }
+    res.end();
+  }
+
+  // ***************************************************************************
+  // GET /api/books
+  // ***************************************************************************
+
+  if (method === 'GET' && path === '/api/books') {
+    try {
+      const { rows: books } = await db.query(`select * from books;`);
+      const json = stringify({ books });
+      res.setHeader('content-type', 'application/json');
+      res.statusCode = 200;
+      res.write(json);
     } catch (err) {
       res.statusCode = 500;
       const data = { error: err.message };
