debe078855bb4b741fbbe029945ec94b9ba729c2
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-01-31 02:00:00 +0000

    implement GET /api

---
+++ server.js
@@ -0,0 +1,37 @@
+import http from 'http';
+import db from './db/db.js';
+
+const HOST = 'localhost';
+const PORT = 8080;
+
+const stringify = json => JSON.stringify(json);
+
+const server = http.createServer((req, res) => {
+  const { method, url: path } = req;
+
+  // ***************************************************************************
+  // GET /api
+  // ***************************************************************************
+
+  if (method === 'GET' && path === '/api') {
+    res.setHeader('content-type', 'application/json');
+    res.statusCode = 200;
+    const data = { message: 'Hello' };
+    try {
+      res.write(stringify(data));
+    } catch (err) {
+      res.statusCode = 500;
+      const data = { error: err.message };
+      res.write(stringify(data));
+    }
+    res.end();
+  }
+});
+
+server.listen(PORT, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`server listening on port: ${PORT}`);
+});
