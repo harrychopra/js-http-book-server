0b5c59cc347481f9410ef681dc0d9600dfe2dab4
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-01-31 05:00:00 +0000

    implement GET /api/books/:bookId

--- server.js
+++ server.js
@@ -65,6 +65,40 @@ const server = http.createServer(async (req, res) => {
     }
     res.end();
   }
+
+  // ***************************************************************************
+  // GET /api/books/:bookId
+  // ***************************************************************************
+
+  const reGetBooksById = /^\/api\/books\/(\d+)$/;
+  const matchGetBooksById = path.match(reGetBooksById);
+
+  if (method === 'GET' && matchGetBooksById.length === 2) {
+    const bookId = matchGetBooksById[1];
+
+    try {
+      const { rows } = await db.query(`--sql
+        select * from books where id = ${bookId};`);
+
+      res.setHeader('content-type', 'application/json');
+
+      let json;
+      if (rows.length === 0) {
+        res.statusCode = 404;
+        json = stringify({ message: 'not found' });
+      } else {
+        res.statusCode = 200;
+        json = stringify({ book: rows[0] });
+      }
+
+      res.write(json);
+    } catch (err) {
+      res.statusCode = 500;
+      const data = { error: err.message };
+      res.write(stringify(data));
+    }
+    res.end();
+  }
 });
 
 server.listen(PORT, err => {
